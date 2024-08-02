2edaed938b41b7854bfd75982977127fb83bd01c
KARAMVEER SINGH <91724375+karam00736@users.noreply.github.com>
2024-08-02 21:53:18 +0530

    Add files via upload
    task is finished

---
+++ java.js
@@ -0,0 +1,9 @@
+let number = [5,10];
+for(let numbers of number){
+  if(numbers % 2 == 0){
+    console.log("The number " + numbers + " is even.")
+  }
+  else{
+    console.log("The number " + numbers + " is odd.")
+  }
+};
